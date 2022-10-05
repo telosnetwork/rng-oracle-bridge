bf330a0c230dbd6ec8092f9518c70fa8c0cd9040
tomtomcrypto <titiomm@gmail.com>
2022-10-05 16:14:11 +0000

    added max callback gas value to deploy

--- evm/deploy/RNGOracleBridge.js
+++ evm/deploy/RNGOracleBridge.js
@@ -5,7 +5,7 @@ module.exports = async ({getNamedAccounts, deployments}) => {
 
     const bridge = await deploy('RNGOracleBridge', {
         from: deployer,
-        args: ["1000000000000000000", 10, 10, "0x63c910d38a4717abe48f923d873314b9260e6dab", "0x648ac5a8c4E1ae5A93cd5BeDF143B095B8c49a2a"],
+        args: ["1000000000000000000", 10, 10, "900000000", "0x63c910d38a4717abe48f923d873314b9260e6dab", "0x648ac5a8c4E1ae5A93cd5BeDF143B095B8c49a2a"],
     });
 
     console.log("Bridge deployed to:", bridge.address);
